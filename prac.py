cbe66a5a449d2b2d33f8155de5d7e8cab00a6669
bilawalwadani <bilawalbarkatali@gmail.com>
2025-03-26 12:23:50 +0500

    Added Firebase for cross-device data persistence

---
+++ prac.py
@@ -0,0 +1,13 @@
+import numpy as np
+mylst=[1,2,3,4,5]
+arr=np.array(mylst)
+
+print(type(arr))
+arr.shape
+
+mylist1=[1,2,3]
+mylist2=[4,5,6]
+mylist3=[7,8,9]
+
+arrq=np.array([mylist1,mylist2,mylist3])
+print(arrq)
